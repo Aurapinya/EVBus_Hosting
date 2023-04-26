9a1330bfa3234d666336348ecd9accf333832467
aurapinya jamsree <aurapinya.jams@dome.tu.ac.th>
2023-04-26 22:31:09 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import {BrowserRouter, Routes, Route,} from 'react-router-dom';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Routes>
+      {/* <Route path="/" element={<Pre/>}/> */}
+      <Route path="/" element={<Login/>}/>
+      <Route path="/signup" element={<Signup/>}/>
+      
+    </Routes>
+  </BrowserRouter>
+  );
+};
+
+export default App;
